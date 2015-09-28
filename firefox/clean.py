1d1f4ecc17c396e281608d1215c86954ab6d1cdb
Ramiro Gómez <code@ramiro.org>
2015-09-28 11:23:43 +0200

    Fix #1: fetch title and url using lassie. Fix #2: group link list by bookmark parent hierarchy.

--- firefox/clean.py
+++ firefox/clean.py
@@ -2,8 +2,11 @@
 # coding: utf-8
 import argparse
 import json
-import requests
 import sys
+
+import requests
+import requests_cache
+import lassie
 
 
 parser = argparse.ArgumentParser(description='Create HTML link list for given tag.')
@@ -11,6 +14,8 @@ parser.add_argument('bmfile', help='Bookmarks file in JSON list format.')
 args = parser.parse_args()
 
 not_ok = []
+requests_cache.configure('../cache/requests')
+
 
 with open(args.bmfile, 'r') as f:
     data = json.load(f)
@@ -19,15 +24,21 @@ for i, b in enumerate(data['bookmarks']):
     url = b['url']
     if not url.startswith(('http', 'https')):
         continue
+
     print('#{}: {}'.format(i, url))
     try:
-        b['status'] = requests.get(url).status_code
+        b['status'] = requests.head(url).status_code
     except Exception as err:
         print('Request failed: {}'.format(err))
         continue
+
     if b['status'] != 200:
         not_ok.append(b)
         del b
+    else:
+        summary = lassie.fetch(url)
+        b['title'] = summary.get('title', b['title'])
+        b['url'] = summary.get('url', b['url'])
 
 with open(args.bmfile, 'w') as f:
     json.dump(data, f)
--- firefox/tag2md.py
+++ firefox/tag2md.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import sys
+from collections import defaultdict
 
 
 parser = argparse.ArgumentParser(description='Create HTML link list for given tag.')
@@ -11,19 +12,24 @@ parser.add_argument('tag', help='The tag to generate a link list for.')
 args = parser.parse_args()
 
 bookmarks = []
+markdown = ''
+tag_groups = defaultdict(list)
 tpl_item = '* [{}]({})'
 
 with open(args.bmfile, 'r') as f:
     data = json.load(f)
 
 for b in data['bookmarks']:
-    if args.tag not in b['parents']:
+    if not b['url'].startswith(('http', 'https')) or args.tag not in b['parents']:
         continue
-    bookmarks.append(tpl_item.format(b['title'], b['url']))
+    tag_groups[' • '.join(b['parents'])].append(tpl_item.format(b['title'], b['url']))
 
-if not bookmarks:
+if not tag_groups:
     print('Tag not found: %s' % args.tag)
     sys.exit()
 
+for group, links in tag_groups.items():
+    markdown += '## {}\n\n{}\n\n'.format(group, '\n'.join(sorted(links)))
+
 with open('../md/' + args.tag + '_list.md', 'w') as f:
-    f.write('\n'.join(bookmarks))
+    f.write(markdown)
